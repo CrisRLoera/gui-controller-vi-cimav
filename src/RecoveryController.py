e32551948d7195c48931f8dca2db87ec6d6984e9
CrisRLoera <rcristianrubi@gmail.com>
2024-07-12 18:37:13 -0600

    UnFix: Restore not posible

--- src/RecoveryController.py
+++ src/RecoveryController.py
@@ -52,6 +52,7 @@ class RecoveryController:
                 print(self.file)
         except FileNotFoundError:
             self.gen_empty_recovery_file()
+            print("No abrio archivo")
 
     def checkRecovery(self):
         limit_for_recovery = 5
@@ -74,28 +75,13 @@ class RecoveryController:
                     self.host.state_controller.task_num = self.file['task num']
                     self.host.state_controller.recoverTask(self.file['stack'])
                     self.host.state_controller.changeOutputs(self.file['output1'],self.file['output2'],self.file['output3'])
-                    try:
-                        self.host.state_screen.changeCurrentProgram()
-                        self.host.state_screen.run_current_program()
-                        self.host.state_controller.restart_flow()
-                        if self.host.isConnected() and self.host.state_screen.current_program['interrupt']!= None:
-                            if self.host.state_screen.current_program["interrupt"] == True and self.host.state_screen.current_program ["responsible"] != None:
-                                self.host.email_controller.send_interruption_email(self.host.state_screen.current_program['name'],self.host.state_screen.current_program ["responsible"])
-                    except:
-                        print("Recovery is not posible")
-                        self.host.state_screen.current_program = {'number': None, 'name': None, 'steps': None}
-                        self.host.state_screen.current_step_number = None
-                        self.host.state_screen.output_state1 = False
-                        self.host.state_screen.output_state2 = False
-                        self.host.state_screen.output_state3 = False
-                        self.host.state_screen.program_jumps_left = None
-                        self.host.state_screen.time_left = None
-                        self.host.state_controller.task_num = 0 
-                        self.state.current_step_number = 0
-                        self.host.state_controller.output1_on_time=None
-                        self.host.state_controller.output2_on_time=None
-                        self.host.state_controller.output3_on_time=None
+                    self.host.state_screen.changeCurrentProgram()
+                    self.host.state_screen.run_current_program()
+                    self.host.state_controller.restart_flow()
+                    if self.host.isConnected() and self.host.state_screen.current_program['interrupt']!= None:
+                        if self.host.state_screen.current_program["interrupt"] == True and self.host.state_screen.current_program ["responsible"] != None:
+                            self.host.email_controller.send_interruption_email(self.host.state_screen.current_program['name'],self.host.state_screen.current_program ["responsible"])
+                 
 
-                        self.stack = [None]
-                        self.host.update_Screen()
+
 
